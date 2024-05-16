44526a037ebafcadeee236e88ffa605fffbd4d81
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-05-16 20:53:24 +0200

    Update PyToExeUI.py

--- python-script/PyToExeUI.py
+++ python-script/PyToExeUI.py
@@ -3,24 +3,27 @@ import sys
 import json
 import traceback
 import shutil
+import psutil
+import win32gui
+import win32process
+import win32con
 
 from PyToExe_ui import *
 from Function_QThread import *
 from Language_init_Chinese import *
 from Language_init_English import *
 
-from PyQt5.QtWidgets import QFileDialog, QMessageBox, QPushButton, QDialog, QListWidget, QHBoxLayout, QPushButton, QVBoxLayout, QSizePolicy, QFrame, QSpacerItem, QInputDialog, QLabel, QCheckBox, QRadioButton, QListWidgetItem, QTextBrowser
+from PyQt5.QtWidgets import  QFileDialog, QMessageBox, QPushButton, QDialog, QListWidget, QHBoxLayout, QPushButton, QVBoxLayout, QSizePolicy, QFrame, QSpacerItem, QInputDialog, QLabel, QCheckBox, QRadioButton, QListWidgetItem, QTextBrowser
 from PyQt5.QtGui import QTextCursor, QDesktopServices, QIcon, QPixmap
 from PyQt5.QtCore import Qt, QUrl, QByteArray
+import pygetwindow as gw
 
-
+# ****************************************初始化全局参数****************************************
 workspace_path = os.getcwd()
 exe_folder_path = os.path.dirname(sys.argv[0])
 icon_data = '''<svg version="1.1" id="svg1" width="400" height="400" viewBox="0 0 400 400" sodipodi:docname="PyToExe.svg" inkscape:version="1.3.2 (091e20e, 2023-11-25, custom)" inkscape:export-filename="PyToExe.png" inkscape:export-xdpi="96" inkscape:export-ydpi="96" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"> <defs id="defs1"> <linearGradient id="swatch6" inkscape:swatch="solid"> <stop style="stop-color:#000000;stop-opacity:1;" offset="0" id="stop6" /> </linearGradient><linearGradient id="swatch35" inkscape:swatch="solid"> <stop style="stop-color:#f54d00;stop-opacity:1;" offset="0" id="stop35" /> </linearGradient> <linearGradient inkscape:collect="always" xlink:href="#swatch35" id="linearGradient35" x1="263.0344" y1="195.86606" x2="400.23287" y2="195.86606" gradientUnits="userSpaceOnUse" /> </defs> <sodipodi:namedview id="namedview1" pagecolor="#ffffff" bordercolor="#000000" borderopacity="0.25" inkscape:showpageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:deskcolor="#d1d1d1" inkscape:zoom="1.5832414" inkscape:cx="154.11421" inkscape:cy="222.01289" inkscape:window-width="1920" inkscape:window-height="1009" inkscape:window-x="2552" inkscape:window-y="-8" inkscape:window-maximized="1" inkscape:current-layer="g1" /> <g inkscape:groupmode="layer" inkscape:label="Image" id="g1"> <path id="path1" style="fill:#ef9e01;fill-opacity:1;stroke:#000000;stroke-opacity:1;stroke-width:2;stroke-dasharray:none" d="M 190.73633,27.681641 A 172.93973,172.93973 0 0 0 121.74609,37.525391 172.93973,172.93973 0 0 0 18.384766,259.18359 172.93973,172.93973 0 0 0 240.04492,362.54492 172.93973,172.93973 0 0 0 351.23047,172.44141 l -38.29688,13.9375 A 129.19458,133.82138 70 0 1 224.75391,322.14844 129.19458,133.82138 70 0 1 54.816406,246.51562 129.19458,133.82138 70 0 1 136.37891,79.34375 129.19458,133.82138 70 0 1 281.98633,115.30078 l 0.002,0.002 c 0.0203,0.0114 0.0405,0.0243 0.0606,0.0352 0.0403,0.0238 0.0798,0.046 0.11719,0.0742 0.007,0.005 0.009,0.0125 0.0156,0.0176 0.005,0.002 0.0105,0.004 0.0156,0.006 0.0358,0.009 0.0663,0.0309 0.0976,0.0488 0.004,0.003 0.009,0.005 0.0117,0.006 0.002,0.002 0.008,0.004 0.0176,0.0117 0.006,0.004 0.009,0.0113 0.0137,0.0156 l 0.002,0.002 c 0.002,0.002 0.005,8.5e-4 0.008,0.002 0.0241,0.008 0.0442,0.024 0.0644,0.0391 h 0.002 c 0.0101,10e-4 0.0202,0.003 0.0312,0.006 0.0324,0.0107 0.0592,0.0325 0.0859,0.0527 0.009,0.007 0.0185,0.0141 0.0254,0.0215 0.0153,0.005 0.0291,0.0103 0.0449,0.0137 0.0248,0.008 0.0512,0.0139 0.0762,0.0215 0.0267,0.009 0.0513,0.0202 0.0781,0.0293 0.0301,0.0117 0.0593,0.0269 0.084,0.0469 a 129.19458,133.82138 70 0 0 -0.0293,-0.0332 20.874001,20.874001 0 0 0 18.125,1.92187 20.874001,20.874001 0 0 0 12.47851,-26.755854 20.874001,20.874001 0 0 0 -0.76953,-1.753907 172.93973,172.93973 0 0 0 -121.9082,-61.449218 z" /> <path id="rect23-1" style="fill:#2525ff;fill-opacity:1;stroke-width:2;stroke:url(#linearGradient35);stroke-opacity:1;stroke-dasharray:none" d="m 268.06954,198.3686 0.0123,-0.009 -3.98989,22.17486 0.12876,0.18526 20.18849,3.63248 47.31514,-32.88487 c 3.34059,-2.32178 7.89731,-1.49973 10.21918,1.8404 l 32.8852,47.31337 20.19025,3.6328 0.14362,-0.0998 4.01273,-22.30189 -49.54068,-71.27972 c -2.32181,-3.34063 -6.88069,-4.16091 -10.22126,-1.8391 l -71.33385,49.57833 z" /> <text xml:space="preserve" style="font-size:202.244px;line-height:0px;letter-spacing:0px;fill:#e6e6e6;fill-opacity:1;stroke:#000000;stroke-width:5.41662;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:stroke fill markers" x="-6.7089086" y="363.74023" id="text29-2"><tspan id="tspan29-2" x="-6.7089086" y="363.74023" sodipodi:role="line" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:202.244px;line-height:0px;font-family:Arial;-inkscape-font-specification:'Arial Bold';letter-spacing:0px;fill:#e6e6e6;fill-opacity:1;stroke:#000000;stroke-width:5.41662;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:stroke fill markers">EXE</tspan></text> <text xml:space="preserve" style="font-style:normal;font-variant:normal;font-weight:bold;font-stretch:normal;font-size:231.553px;line-height:0px;font-family:Arial;-inkscape-font-specification:'Arial Bold';letter-spacing:3.4733px;fill:#e6e6e6;fill-opacity:1;stroke:#000000;stroke-width:5.78883;stroke-dasharray:none;stroke-dashoffset:0;stroke-opacity:1;paint-order:stroke fill markers" x="32.131664" y="185.16193" id="text6"><tspan sodipodi:role="line" id="tspan6" x="32.131664" y="185.16193" style="font-size:231.553px;letter-spacing:3.4733px;stroke-width:5.78883">PY</tspan></text> </g> </svg>'''
-# default_icon_path = os.path.join(exe_folder_path, 'PyToExe.ico') 
-# default_icon_path = os.path.join(exe_folder_path, 'resource', 'PyToExe.ico')
 
-# 自寻py文件路径
+# ****************************************自寻py文件路径****************************************
 py_file_name_auto = ''
 if len(sys.argv) > 1 and sys.argv[1].endswith('.py'):
     py_file_path_auto = sys.argv[1]
@@ -32,28 +35,33 @@ else:
         elif not py_file_name_auto and '.py' in name:
             py_file_name_auto = name
 
-# 读取setting文件
+# ****************************************读取setting文件****************************************
+default_setting = {"Language":"简体中文(内置)", "Multiple_Windows":True}
 setting_path = os.path.join(exe_folder_path, 'setting')
-if os.path.exists(setting_path):
+# 检查是否为json格式，并检查是否存在该文件，若不存在则创建默认设置的setting文件
+try:
     with open (setting_path, 'r', encoding='utf-8') as file:
         setting_file = json.load(file)
-else:
-    setting_file = {'Language':'简体中文(内置)'}
+# 不为json格式，则重写setting文件
+except:
+    setting_file = default_setting
     with open (setting_path, 'w', encoding='utf-8') as file:
         json.dump(setting_file, file, ensure_ascii=False, indent=None)
-
-# if os.path.exists(setting_path):
-#     with open (setting_path, 'r', encoding='utf-8') as file:
-#         line = file.readline()
-#         setting_file = {}
-#         while line:
-#             key = line.split('=')[0].strip()
-#             value = line.split('=')[1].strip()
-#             setting_file[key] = value
-#             line = file.readline()
-# else:
-#     with open (setting_path, 'w', encoding='utf-8') as file:
-#         file.write('Language=简体中文(内置)')
+new_setting_file_flag = False
+# 检查缺失项
+for i in default_setting:
+    if not i in setting_file:
+        new_setting_file_flag = True
+        setting_file[i] = default_setting[i]
+# 检查多余项
+for key in setting_file.copy():
+    if key not in default_setting:
+        new_setting_file_flag = True
+        del setting_file[key]
+# 如果存在缺失或者多余项，则更新setting文件
+if new_setting_file_flag == True:
+    with open (setting_path, 'w', encoding='utf-8') as file:
+        json.dump(setting_file, file, ensure_ascii=False, indent=None)
 
 class PyToExeUI(Ui_MainWindow):
     def __init__(self):
@@ -64,12 +72,77 @@ class PyToExeUI(Ui_MainWindow):
         self.parameter_init()
         self.ui_init()
         self.py_to_exe_ui_signal_connection()
+        self.check_and_deal_existed_win()
+        
+    # ****************************************检测是否已运行该程序****************************************
+    def check_and_deal_existed_win(self):
+        try:
+            if self.is_current_script_running() and not bool(setting_file['Multiple_Windows']):
+                self.get_process_windows(self.pid[1])
+                print("当前脚本已经在运行。")
+                sys.exit()
+            else:
+                print("当前脚本未在运行，继续执行其他操作。")
+        except Exception as e:
+            QMessageBox.information(None, ' ', e)
+    
+    def get_process_windows(self, pid):
+        print(pid)
+        for i in gw.getAllWindows():
+            hwnd = i._hWnd
+            _, win_pid = win32process.GetWindowThreadProcessId(hwnd)
+            if pid ==  win_pid:
+                win32gui.ShowWindow(hwnd, win32con.SW_RESTORE)
+                win32gui.SetForegroundWindow(hwnd)
+    
+    def is_current_script_running(self):
+        try:
+            current_exe_path = os.path.abspath(sys.argv[0])
+            current_exe_name = os.path.basename(current_exe_path)
+            self.pid = []
+            # 遍历所有进程，检查是否有相同的 exe 文件在运行
+            exe_count = 0
+            timestamp = []
+            for process in psutil.process_iter(['pid', 'name', 'exe']):
+                # print(process)
+                temp_process_name = process.info['name']
+                temp_process_exe = process.info['exe']
+                temp_process_time = process.create_time()
+                temp_pid = process.pid
+                if temp_process_name.lower() == current_exe_name.lower() and temp_process_exe.lower() == current_exe_path.lower():
+                    exe_count += 1
+                    if not self.pid: # 第一个值进来
+                        self.pid.append(temp_pid)
+                        timestamp.append(temp_process_time)
+                    elif len(self.pid) < 2:
+                        if timestamp[0] < temp_process_time: # 第二个值进来。 如果第二个值比第一个值大，时间晚
+                            self.pid.append(temp_pid)
+                            timestamp.append(temp_process_time)
+                        else:
+                            self.pid.insert(0, temp_pid)
+                            timestamp.insert(0, temp_process_time)
+                    elif timestamp[1] > temp_process_time:
+                        if timestamp[0] > temp_process_time:
+                            timestamp[1] = timestamp[0]
+                            timestamp[0] = temp_process_time
+                            self.pid[1] = self.pid[0]
+                            self.pid[0] = temp_pid
+                        else:
+                            timestamp[1] = temp_process_time
+                            self.pid[1] = temp_process_time
+            if (exe_count // 2) > 1:
+                return True
+            else:
+                return False
+        except Exception as e:
+            QMessageBox.information(None, ' ', e)
     
     # ****************************************初始化****************************************
     # UI界面的初始化
     def ui_init(self):
         self.set_text_init()
         self.Win.pb_Recover.hide()
+        self.Win.progressBar.hide()
         self.Win.pte_FilePath.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.Win.pte_FileName.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.Win.pte_OutputPath.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -79,6 +152,8 @@ class PyToExeUI(Ui_MainWindow):
         self.Win.cbb_LanguageSelect.setFocusPolicy(Qt.NoFocus)
         self.Win.frame_CondaDisplay.hide()
         self.Win.cb_CondaUse.setEnabled(False)
+        self.Win.cb_DisableWindowed.setEnabled(False)
+        self.Win.cb_MultiWin.setChecked(bool(setting_file['Multiple_Windows']))
         # 检查系统是否安装Conda，如果安装了，将Conda按钮置为可用
         if shutil.which("conda"):
             self.Win.pb_CondaSetting.setEnabled(True)
@@ -101,6 +176,7 @@ class PyToExeUI(Ui_MainWindow):
         pixmap_icon = QPixmap()
         pixmap_icon.loadFromData(QByteArray(icon_data.encode()))
         self.WINDOW_ICON = QIcon(pixmap_icon)
+        self.progressBar_value = 0
         self.launch_flag = True
         self.clear_file_flag = True
         # 用于显示参数，Label是项目名，explain是参数含义
@@ -166,6 +242,7 @@ class PyToExeUI(Ui_MainWindow):
         self.Win.pb_Print.clicked.connect(self.print_cmd)
         self.Win.cbb_LanguageSelect.currentTextChanged.connect(self.language_changed)
         self.Win.cb_Tooltips.stateChanged.connect(self.language_update)
+        self.Win.cb_MultiWin.stateChanged.connect(self.multi_windows_changed)
     
     # 界面文字初始化及更改
     def combo_list_init(self):
@@ -235,6 +312,11 @@ class PyToExeUI(Ui_MainWindow):
     def language_update(self):
         self.load_language_json_file()
         self.set_text_init()
+    
+    def multi_windows_changed(self):
+        setting_file['Multiple_Windows'] = self.Win.cb_MultiWin.isChecked()
+        with open (setting_path, 'w', encoding='utf-8') as file:
+            json.dump(setting_file, file, ensure_ascii=False, indent=None)
     
     # ****************************************PLE有输入变化时，数据更新****************************************
     def file_path_changed(self):
@@ -752,3 +834,4 @@ class PyToExeUI(Ui_MainWindow):
         if result == QDialog.Accepted:
             selected_items = list_widget.currentItem().text()
             return selected_items
+    
