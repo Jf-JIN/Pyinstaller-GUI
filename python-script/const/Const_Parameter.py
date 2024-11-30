cd00bea0dcac2ed7cf9c1c53e1046744dbb739c8
JIN <119544344+Jf-JIN@users.noreply.github.com>
2024-11-30 03:44:13 +0100

    增添常量枚举

--- python-script/const/Const_Parameter.py
+++ python-script/const/Const_Parameter.py
@@ -1,8 +1,14 @@
+
 import os
 import sys
+from system.AnalogDefine import AnalogDefine
 
 os.chdir(os.path.dirname(os.path.dirname(__file__)))
 if len(sys.argv) > 1:
     APP_WORKSPACE_PATH = sys.argv[1]
 else:
     APP_WORKSPACE_PATH = os.getcwd()
+
+
+class EnumConst(AnalogDefine):
+    DEFAULT_INSTALLER_COMMANDLINE = '--onefile --console --clean'
