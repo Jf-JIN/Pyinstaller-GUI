bc29e3fc4337e95aeba2d39286246b80c460e9f5
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-02-11 08:33:00 +0100

    新增信号和信号节流

--- python-script/system/Struct_env_info.py
+++ python-script/system/Struct_env_info.py
@@ -1,5 +1,12 @@
 
+from .Signal_Event import EventSignal
+
+from PyQt5.QtCore import QTimer
+
+
 class StructEnvInfo(object):
+    signal_isChanged = EventSignal()
+
     def __init__(self, name):
         self.__name = name
         self.env_name: str = ''
@@ -8,11 +15,22 @@ class StructEnvInfo(object):
         self.version: str = ''
         self.path_error: bool = False  # 给 指定解释器路径 用的, 记录路径是否错误
         self.command_launch: str = ''  # 给 当前环境 用的, 记录启动路径
+        self.__timer_setattr = QTimer()
+        self.__timer_setattr.timeout.connect(self.__emit_signal)
 
     @property
     def name(self) -> str:
         """获取环境名称"""
         return self.__name
 
+    def __setattr__(self, name, value):
+        if hasattr(self, f'_{self.__class__.__name__}__timer_setattr'):
+            self.__timer_setattr.start(100)
+        return super().__setattr__(name, value)
+
     def __str__(self):
-        return f"环境名称: {self.env_name}\n环境路径: {self.env_path}\n环境版本: {self.env_version}\n环境类型: {self.env_type}"
+        return f"环境名称: {self.env_name}\npython路径: {self.path_python}\n环境版本: {self.version}"
+
+    def __emit_signal(self):
+        self.signal_isChanged.emit()
+        self.__timer_setattr.stop()
