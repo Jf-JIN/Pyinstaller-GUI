fca233877fe8cae235c8234f821a5a883bcbeade
JIN <119544344+Jf-JIN@users.noreply.github.com>
2025-02-11 08:31:23 +0100

    部分重构读取部分，但仍需再重构，此文件当前只适配使用Struct_IO

--- python-script/service/Function.py
+++ python-script/service/Function.py
@@ -27,29 +27,29 @@ class FunctionUI(PyToExeUI):
         self.le_output_folder_path.editingFinished.connect(self.write_output_folder_path)
         self.le_output_file_name.editingFinished.connect(self.write_output_file_name)
 
-    def init_python_file_path(self):
-        self.le_input_py_file_path.clear()
-        self.le_output_folder_path.clear()
-        self.le_output_file_name.clear()
-        if len(sys.argv) > 1 and sys.argv[1].endswith(('.py', '.pyw', '.pyd', '.spec')):
-            file_path = sys.argv[1]
-            self.app_workspace_path = os.path.dirname(file_path)
-        elif len(sys.argv) > 1 and sys.argv[1].endswith(('.txt', '.ocl')):
-            self.installer_manager.read_file(sys.argv[1])
-            file_path = self.installer.python_file_path.command
-        elif self.installer.python_file_path.command and os.path.exists(self.installer.python_file_path.command):
-            file_path = self.installer.python_file_path.command
-        else:
-            return
-        self.le_input_py_file_path.setText(file_path)
-        self.le_output_folder_path.setText(os.path.dirname(file_path))
-        self.le_output_file_name.setText(os.path.splitext(os.path.basename(file_path))[0])
-        # 写配置文件
-        self.installer.python_file_path.set_args(file_path)
-        self.setting['last_command'] = self.installer.get_command_line(self.env_struct_current.path_pyinstaller)
-        self.setting_manager.write_file_to_json()
-        # 更新安装器信息
-        self.update_installer_info()
+    # def init_python_file_path(self):
+    #     self.le_input_py_file_path.clear()
+    #     self.le_output_folder_path.clear()
+    #     self.le_output_file_name.clear()
+    #     if len(sys.argv) > 1 and sys.argv[1].endswith(('.py', '.pyw', '.pyd', '.spec')):
+    #         file_path = sys.argv[1]
+    #         self.app_workspace_path = os.path.dirname(file_path)
+    #     elif len(sys.argv) > 1 and sys.argv[1].endswith(('.txt', '.ocl')):
+    #         self.installer_manager.read_file(sys.argv[1])
+    #         file_path = self.installer.python_file_path.command
+    #     elif self.installer.python_file_path.command and os.path.exists(self.installer.python_file_path.command):
+    #         file_path = self.installer.python_file_path.command
+    #     else:
+    #         return
+    #     self.le_input_py_file_path.setText(file_path)
+    #     self.le_output_folder_path.setText(os.path.dirname(file_path))
+    #     self.le_output_file_name.setText(os.path.splitext(os.path.basename(file_path))[0])
+    #     # 写配置文件
+    #     self.installer.python_file_path.set_args(file_path)
+    #     self.setting['last_command'] = self.installer.get_command_line(self.env_struct_current.path_pyinstaller)
+    #     self.setting_manager.write_file_to_json()
+    #     # 更新安装器信息
+    #     self.update_installer_display_info()
 
     def set_input_py_file_path(self):
         file_path = QFileDialog.getOpenFileName(
@@ -57,6 +57,53 @@ class FunctionUI(PyToExeUI):
             Python Files (*.py *.pyw *.pyd *.spec);; 
             Text Files (*.txt);; 
             Output Command Line Files (*.ocl)""")[0]
+        folder_path = ''
+        output_file_name = ''
+        if not file_path:
+            return
+        if file_path.endswith(('.ocl', '.txt')):
+            hasError = self.installer_manager.read_file(file_path)
+            if hasError == 'hasError':
+                self.message.notification('文件错误, 非标准格式')
+                return
+            elif hasError is None:
+                self.message.notification('没有找到pyinstaller命令')
+            file_path = self.installer.python_file_path.command_args
+            if self.installer.output_folder_path.command:
+                folder_path = self.installer.output_folder_path.command_args
+            if self.installer.output_file_name.command:
+                output_file_name = self.installer.output_file_name.command_args
+
+        # self.le_input_py_file_path.setText(file_path)
+        # self.app_workspace_path = os.path.dirname(file_path)
+        # # 写配置文件
+        # self.installer.python_file_path.set_args(file_path)
+        # self.setting['last_command'] = self.installer.get_command_line(self.env_struct_current.path_pyinstaller)
+        # if not self.cb_lock_output_folder.isChecked():
+        #     if not folder_path:
+        #         folder_path = os.path.dirname(file_path)
+        #     self.le_output_folder_path.setText(folder_path)
+        #     self.installer.output_folder_path.set_args(folder_path)
+        # if not self.cb_lock_output_file_name.isChecked():
+        #     if not output_file_name:
+        #         output_file_name = os.path.splitext(os.path.basename(file_path))[0]
+        #     self.le_output_file_name.setText(output_file_name)
+        #     self.installer.output_file_name.set_args(output_file_name)
+        # self.setting_manager.write_file_to_json()
+        # 更新安装器信息
+        self.update_installer_display_info()
+
+    def set_input_file(self, file_path: str):
+        if os.path.exists(file_path) and file_path.endswith(('.ocl', '.txt')):
+            hasError = self.installer_manager.read_file(file_path)
+            if hasError == 'hasError':
+                self.message.notification('文件错误, 非标准格式')
+                return
+            elif hasError is None:
+                self.message.notification('没有找到pyinstaller命令')
+        elif os.path.exists(file_path) and file_path.endswith(('.py','.pyd','.pyw','.spec')):
+            self.installer
+
         folder_path = ''
         output_file_name = ''
         if not file_path:
@@ -74,6 +121,7 @@ class FunctionUI(PyToExeUI):
 
         self.le_input_py_file_path.setText(file_path)
         self.app_workspace_path = os.path.dirname(file_path)
+        self.original_installer = self.installer.copy()
         # 写配置文件
         self.installer.python_file_path.set_args(file_path)
         self.setting['last_command'] = self.installer.get_command_line(self.env_struct_current.path_pyinstaller)
@@ -89,7 +137,7 @@ class FunctionUI(PyToExeUI):
             self.installer.output_file_name.set_args(output_file_name)
         self.setting_manager.write_file_to_json()
         # 更新安装器信息
-        self.update_installer_info()
+        self.update_installer_display_info()
 
     def set_output_folder_path(self):
         folder_path = QFileDialog.getExistingDirectory(self, '选择输出文件夹', self.app_workspace_path)
@@ -98,7 +146,7 @@ class FunctionUI(PyToExeUI):
         self.le_output_folder_path.setText(folder_path)
         self.installer.output_folder_path.set_args(folder_path)
         # 更新安装器信息
-        self.update_installer_info()
+        self.update_installer_display_info()
 
     def write_output_folder_path(self):
         folder_path = self.le_output_folder_path.text()
@@ -106,7 +154,7 @@ class FunctionUI(PyToExeUI):
             return
         self.installer.output_folder_path.set_args(folder_path)
         # 更新安装器信息
-        self.update_installer_info()
+        self.update_installer_display_info()
 
     def set_output_file_name(self):
         file_name: str = self.le_output_file_name.text()
@@ -114,7 +162,7 @@ class FunctionUI(PyToExeUI):
             return
         self.installer.output_file_name.set_args(file_name)
         # 更新安装器信息
-        self.update_installer_info()
+        self.update_installer_display_info()
 
     def write_output_file_name(self):
         file_path: str = self.le_input_py_file_path.text()
@@ -123,4 +171,4 @@ class FunctionUI(PyToExeUI):
             return
         self.installer.output_file_name.set_args(output_file_name)
         # 更新安装器信息
-        self.update_installer_info()
+        self.update_installer_display_info()
